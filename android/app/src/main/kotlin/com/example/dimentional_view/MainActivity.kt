a8f74ae1b1ef9a93b912091b457dd6afa37ea517
flutterdev-ravan <velmurugan1211r@gmail.com>
2023-07-24 14:31:40 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/dimentional_view/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dimentional_view
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/HomePage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          centerTitle: true,
+          title: const Text(
+            "3 D   V I E W  I N  F L U T T E R",
+            style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+          ),
+        ),
+        body: const SizedBox(
+          child: ModelViewer(
+            src: 'assets/3d/DinklageLikenessSculpt.glb',
+            backgroundColor: Color.fromARGB(0xFF, 0xEE, 0xEE, 0xEE),
+            alt: 'A 3D model of an astronaut',
+            ar: true,
+            arModes: ['scene-viewer', 'webxr', 'quick-look'],
+            autoRotate: true,
+            disableZoom: false,
+          ),
+        ));
+  }
+}
